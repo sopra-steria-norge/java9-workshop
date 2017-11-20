325bf3dc6ae247cce232cf31bfcdec7df53adef3
teide <tee@soprasteria.com>
2017-11-20 23:51:10 +0100

    Jlink-oppgave

---
+++ oppgaver/5/src/main/java/no/soprasteria/workshop/Test.java
@@ -0,0 +1,12 @@
+package no.soprasteria.workshop;
+
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Test {
+
+    public static void main(String[] args) {
+        Logger.getLogger("Test").log(Level.FINE, "Hei");
+    }
+}
